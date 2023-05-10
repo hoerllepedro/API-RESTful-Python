1de22bc0a1f31483203a1b9e3869bbf434ab1565
Pedro Hoerlle <98281997+hoerllepedro@users.noreply.github.com>
2023-05-09 21:08:15 -0300

    Add files via upload

---
+++ CRUD.py
@@ -0,0 +1,106 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+class Material:
+    def __init__(self, id, name, qtde):
+        self.id = id
+        self.name = name
+        self.qtde = qtde
+
+    @staticmethod
+    def buscar_material_por_id(id):
+        for material in materiais:
+            if material.id == id:
+                return material
+        return None
+
+
+materiais = []
+material_id_counter = 0
+
+
+@app.route('/materiais', methods=['GET'])
+def listar_materiais():
+    response = []
+    for i, material in enumerate(materiais):
+        data = {
+            'id': material.id,
+            'name': material.name,
+            'qtde': material.qtde
+        }
+        response.append(data)
+    
+    return jsonify(response), 200
+
+@app.route('/materiais', methods=['POST'])
+def criar_material():
+    data = request.get_json()
+    nome = data['material']['name']
+    qtde = data['material']['qtde']
+    
+    global material_id_counter
+    material_id_counter += 1
+    material = Material(material_id_counter, nome, qtde)
+    materiais.append(material)
+    
+    response = {'message': 'Material criado com sucesso!'}
+    return jsonify(response), 201
+
+@app.route('/materiais/<int:id>', methods=['GET'])
+def buscar_material(id):
+    material = Material.buscar_material_por_id(id)
+    if material:
+        response = {'material': {
+            'id': material.id,
+            'name': material.name,
+            'qtde': material.qtde
+        }}
+        return jsonify(response), 200
+    else:
+        return jsonify({'message': 'Material não encontrado'}), 404
+
+ 
+@app.route('/materiais/<int:id>', methods=['PUT'])
+def alterar_material(id):
+    data = request.get_json()
+    nome = data['material']['name']
+    qtde = data['material']['qtde']
+    
+    material = Material.buscar_material_por_id(id)
+    if material:
+        material.name = nome
+        material.qtde = qtde
+        
+        response = {
+            'material': {
+                'id': material.id,
+                'name': material.name,
+                'qtde': material.qtde
+            }
+        }
+        return jsonify(response), 200
+    else:
+        return jsonify({'message': 'Material não encontrado'}), 404
+
+@app.route('/materiais/<int:id>', methods=['DELETE'])
+def remover_material(id):
+    material = Material.buscar_material_por_id(id)
+    if material:
+        materiais.remove(material)
+        response = []
+        for i, material in enumerate(materiais):
+            data = {
+                'id': material.id,
+                'name': material.name,
+                'qtde': material.qtde
+            }
+            response.append(data)
+        return jsonify(response), 200
+    else:
+        return jsonify({'message': 'Material não encontrado'}), 404
+
+
+
+if __name__ == '__main__':
+    app.run()
